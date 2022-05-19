1975378a9ad752d8fd4d70b97b63285685125660
Uvindu1 <uvinducs153@gmail.com>
2022-05-19 06:58:06 +0530

    snak game part 1

---
+++ snakGame.py
@@ -0,0 +1,106 @@
+import math
+import random
+
+import cv2
+import numpy as np
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+class snakGame:
+    def __init__(self, pathFood):
+        self.points = [] # all points of the snake
+        self.lengths = [] # distance between each point
+        self.currentLenght = 0 # total length of the snake
+        self.allowedLenght = 150 # total allowed length
+        self.previousHead = 0, 0 # previous head point
+
+        self.imgFood = cv2.imread(pathFood, cv2.IMREAD_UNCHANGED)
+        self.hFood, self.wFood, _ = self.imgFood.shape
+        self.foodPoint = 0, 0
+        self.randomFoodLocation()
+
+    def randomFoodLocation(self):
+        self.foodPoint = random.randint(100, 1000), random.randint(100, 600)
+
+
+    def update(self, imgMain, currentHead):
+        px, py = self.previousHead
+        cx, cy = currentHead
+        self.points.append([cx, cy])
+        distance = math.hypot(cx - px, cy - py)
+        self.lengths.append(distance)
+        self.currentLenght += distance
+        self.previousHead = cx, cy
+
+        #length Reduction
+        if self.currentLenght > self.allowedLenght:
+            for i,length in enumerate(self.lengths):
+                self.currentLenght -= length
+                self.lengths.pop(i)
+                self.points.pop(i)
+                if self.currentLenght < self.allowedLenght:
+                    break
+
+        #Draw Snake
+        if self.points:
+            for i, point in enumerate(self.points):
+                if i !=0:
+                    cv2.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
+            cv2.circle(imgMain, pointIndex, 20, (200, 0, 200), cv2.FILLED)
+
+        #Drow food
+        rx, ry = self.foodPoint
+        imgMain = cvzone.overlayPNG(imgMain, self.imgFood, (rx - self.wFood // 2, ry - self.hFood // 2))
+        return imgMain
+
+game = snakGame("Donat.png")
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    hands, img = detector.findHands(img,flipType=False)
+
+    if hands:
+        lmList = hands[0]['lmList']
+        pointIndex = lmList[8][0:2]
+        img = game.update(img,pointIndex)
+
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
